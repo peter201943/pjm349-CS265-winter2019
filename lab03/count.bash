140efb329dc878e5c8cfbfdf5982a3dc10574ad8
pjm349 <pjm349@drexel.edu>
2019-01-30 22:35:00 -0500

    2 down 1 left!

--- lab03/count.bash
+++ lab03/count.bash
@@ -7,34 +7,16 @@
 #CS 265
 #30 Jan 2019
 #
-#Write a script called count.bash that, for each regular file in the working directory, prints the filename, the # of lines, and the # of words to stdout, like this without using FIND. Must support spaced filenames: 
-#		breadIsDangerous.txt 73 431 
-#		spellExample 5 21
+#Write a script called count.bash that, for each regular file in the working directory, prints the filename, the # of lines, and the # of words to stdout, like this without using FIND. Must support spaced filenames:
+#breadIsDangerous.txt 73 431
+#spellExample 5 21
 #
 #INPUT: Directory command is called in
 #OUTPUT: Filename, Line Count, Word Count
 #
 
-#SEARCHING
-
-
-
-#IDENTIFYING FOR EACH FILE
-
-
-	#NAME
-	
-	
-	#LINE COUNT
-$lines = wc -l filename
-	
-	#WORD COUNT
-$words = wc -w filename
-
-#RETURNING INFORMATION
-
-echo "$name $lines $words"
-
-#CLOSING THE PROGRAM
-
-exit $1
+for file in $(ls -p | grep -v /); do
+    lines=$(wc -l <$file)
+    words=$(wc -w <$file)
+    echo $file $lines $words
+done
