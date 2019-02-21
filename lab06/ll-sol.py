837dc88822acd514b2285146fc42d2c16a546d5b
pjm349 <pjm349@drexel.edu>
2019-02-20 22:15:28 -0500

    Lab 6 Complete

---
+++ lab06/ll-sol.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# linked-list.py - an example of a singly-linked list
+#
+# Kurt Schmidt
+# Oct '06
+#
+# NOTE: Hobbled.  Does not run.  Do not refer to this as an example Python
+#		program
+#
+# EDITOR: tabstop=2
+#
+
+def main() :
+
+	L = [24, None]
+
+	t = [13, None]
+	t[1] = L
+	L = t
+
+	t = [28, None]
+	t[1] = L[1]
+	L[1] = t
+
+	t = [3, None]
+	p = L
+	while p != None :
+		q = p
+		p = p[1]
+
+	if p == L :
+		L = t
+	else :
+		q[1] = t
+
+	print L
+
+if __name__ == "__main__" :   # then this was NOT included in another file
+	main()
+
+
---
+++ lab06/sNode.h
@@ -0,0 +1,21 @@
+/*
+ * sNode.h - a struct, used in singly-linked lists of strings
+ *
+ * Kurt Schmidt
+ * Feb '06
+ *
+ * EDITOR
+ *	tabstop=2
+ *  cols=80
+ *
+ */
+
+#include "stdlib.h"
+
+typedef struct sNode
+{
+	char *data;
+	struct sNode *next;
+} sNode;
+
+typedef sNode* List;
