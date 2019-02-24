35b1f03c1af493ed06b8553430f6e7128637444b
pjm349 <pjm349@drexel.edu>
2019-02-24 17:38:56 -0500

    Some bugs fixed

--- assn2/calc.py
+++ assn2/calc.py
@@ -23,119 +23,294 @@ import sys
 
 
 
+'''SECTION ONE HALF: OPERATORS'''
+
+#Adds two operands from the stack, uses next operand if error.
+def stackAdd(x,y,z):
+    try:
+        stack_sum = str(int(y) + int(x))
+    except:
+        try:
+            stack_sum = str(int(z) + int(x))
+        except:
+            stack_sum = str(int(y) + int(z))
+    return stack_sum
+
+#Subtracts two operands from the stack, uses next operand if error.
+def stackSub(x,y,z):
+    try:
+        stack_difference = str(int(y) - int(x))
+    except:
+        try:
+            stack_difference = str(int(z) - int(x))
+        except:
+            stack_difference = str(int(z) - int(y))
+    return stack_difference
+
+#Multiplies two operands from the stack, uses next operand if error.
+def stackMul(x,y,z):
+    try:
+        stack_product = str(int(y) * int(x))
+    except:
+        try:
+            stack_product = str(int(z) * int(x))
+        except:
+            stack_product = str(int(y) * int(z))
+    return stack_product
+
+#Divides two operands from the stack, uses next operand if error.
+def stackDiv(x,y,z):
+    try:
+        stack_quotient = str(int(y) / int(x))
+    except:
+        try:
+            stack_quotient = str(int(z) / int(x))
+        except:
+            stack_quotient = str(int(z) / int(y))
+    return stack_quotient
+
+#Modulates two operands from the stack, uses next operand if error.
+def stackMod(x,y,z):
+    try:
+        stack_modulus = str(int(y) % int(x))
+    except:
+        try:
+            stack_modulus = str(int(z) % int(x))
+        except:
+            stack_modulus = str(int(z) % int(y))
+    return stack_modulus
+
+
+
+
 '''SECTION ONE: CALCULATOR FUNCTIONS'''
 
 #Converts an infix notation string to a postfix notation string by inspecting each token of the equation.
+#A whitespace is added after each postfix appenditure to ensure ease of legibility.
 def infix2postfix(infix):
+    flag = False
     postfix = ""
     stack = []
     stack.append("(")
-    postfix.append(")")
-    for line in infix:
-        for token in line:
-            
-            #Ignores Whitespaces.
-            if token == " ":
-                pass
-            
-            #Opening Parenthesis added "as-is".
-            elif token == "(":
-                stack.append(token)
-                
-            #Closing parenthesis looks for an opening parenthesis and bind the tokens within.
-            elif token == ")":
-                done = False
-                while not done:
-                    element = stack.pop()
-                    if element == "(":
-                        break
-                    elif element != "(":
-                        postfix.append(element)
-                        
-            #Integers are simply appended to the postfix.
-            elif isinstance(token, int):
-                postfix.append(token)
-                
-            #Lowest level operators ignore all hierarchy.
-            elif token == "+" or "-":
-                done = False
-                while not done:
-                    element = stack.pop()
-                    if element == "(":
-                        stack.append(element)
-                        break
-                    else:
-                        postfix.append(element)
-                        
-            #Higher level operators are dependent on the presence of lowest level operators.
-            elif token == "*" or "/" or "%":
-                done = False
-                while not done:
-                    element = stack.pop()
-                    if element == "(":
-                        stack.append(element)
-                        break
-                    elif element == "-" or "+":
-                        stack.append(element)
-                        break
-                    else:
-                        postfix.append(element)
-
-            #The unlikely event an unfriendly character is encountered.
+    infix += ")"
+    for token in infix:
+        
+        #To resolve whitespace issue. Ensures numbers do not split apart.
+        if flag == True:
+            if token.isdigit():
+                postfix += token
+                token = " "
             else:
-                print("A Critical Error has Occurred on line 86. Reason: Unidentifiable Token. Token: " + str(token))
+                flag = False
+                postfix += " "
+
+        #Ignores Whitespaces.
+        if token == " ":
+            pass
+        
+        #Ignores Newlines. (You have no idea how infuriating this was to debug).
+        elif token == "\n":
+            pass
+        
+        #Opening Parenthesis added "as-is".
+        elif token == "(":
+            stack.append(token)
+            
+        #Closing parenthesis looks for an opening parenthesis and binds the tokens within.
+        elif token == ")":
+            done = False
+            while not done:
+                element = stack.pop()
+                if element == "(":
+                    break
+                elif element != "(":
+                    postfix += element + " "
+                    
+        #Integers are simply appended to the postfix.
+        elif token.isdigit():
+            postfix += token
+            flag = True
+            
+        #Lowest level operators ignore all hierarchy.
+        elif token == "+" or "-":
+            done = False
+            while not done:
+                element = stack.pop()
+                if element == "(":
+                    stack.append(element)
+                    break
+                else:
+                    postfix += " " + element + " "
+            stack.append(token)
+            
+        #Higher level operators are dependent on the presence of lowest level operators.
+        elif token == "*" or "/" or "%":
+            done = False
+            while not done:
+                element = stack.pop()
+                if element == "(":
+                    stack.append(element)
+                    break
+                elif element == "-" or "+":
+                    stack.append(element)
+                    break
+                else:
+                    postfix += " " + element + " "
+            stack.append(token)
+
+        #The unlikely event an unfriendly character is encountered.
+        else:
+            print("A Critical Error has Occurred on line 104. Reason: Unidentifiable Token. Token: " + token)
                 
     #In the event the stack is not empty, notify the user.
     if len(stack) > 0:
         stack_string = ""
         for element in stack:
-            stack_string.append(element)
-        print("A Critical Error has Occurred on line 93. Reason: End of Expression Parsing, but Stack is not Empty. Stack: " + stack_string + " Postfix: " + postfix)
-        return
+            stack_string += element + " "
+        print("A Critical Error has Occurred on line 111. Reason: End of Expression Parsing, but Stack is not Empty. Stack: " + stack_string + " Postfix: " + postfix)
+        return postfix
     else:
         return postfix
 
 #Evaluates a postfix notation string to an integer.
 def evalPostfix(postfix):
-    answer = 0
     stack = []
     x = ""
     y = ""
-    for token in postfix:
-        if isinstance(token, int):
+    while (len(postfix) != 0):
+        token = postfix.pop()
+        '''
+        #A completely hacked together solution to an irritating problem, which would require massive overhaul.
+        if token.isdigit():
+            pass
+        else:
+            next_token = postfix.pop()
+            if next_token.isdigit():
+                postfix.append(next_token)
+                
+            next_next_token = postfix.pop()
+            if next_next_token.isdigit():
+                postfix.append(next_next_token)
+                
+            #Eliminates redundant operators.
+            if (not token.isdigit()) and (next_token == token):
+                pass
+            
+            #Eliminates redundant operators.
+            if (not token.isdigit()) and (next_next_token == token):
+                pass'''
+            
+        #Skipping the whitespaces.
+        if token == " ":
+            pass
+    
+        #Integers added "as-is" to stack.
+        elif token.isdigit():
             stack.append(token)
+            
+            #Operators have special rules. They keep popping until an actual value is found.
+            #I should probably make these things a class... Sorta did that.
         elif token == "*":
-            x = stack.pop()
-            y = stack.pop()
-            stack.append(str(int(x) * int(y)))
+            z = 0
+            x = postfix.pop()
+            if x == " ":
+                x = postfix.pop()
+            y = postfix.pop()
+            if y == " ":
+                y = postfix.pop()
+            try:
+                z = postfix.pop()
+                if z == " ":
+                    z = postfix.pop()
+                postfix.append(z)
+            except:
+                z = 0
+            stack.append(stackMul(x,y,z))
+        
         elif token == "/":
-            x = stack.pop()
-            y = stack.pop()
-            stack.append(str(int(x) / int(y)))
+            z = 0
+            x = postfix.pop()
+            if x == " ":
+                x = postfix.pop()
+            y = postfix.pop()
+            if y == " ":
+                y = postfix.pop()
+            try:
+                z = postfix.pop()
+                if z == " ":
+                    z = postfix.pop()
+                postfix.append(z)
+            except:
+                z = 0
+            stack.append(stackDiv(x,y,z))
+        
         elif token == "%":
-            x = stack.pop()
-            y = stack.pop()
-            stack.append(str(int(x) % int(y)))
+            z = 0
+            x = postfix.pop()
+            if x == " ":
+                x = postfix.pop()
+            y = postfix.pop()
+            if y == " ":
+                y = postfix.pop()
+            try:
+                z = postfix.pop()
+                if z == " ":
+                    z = postfix.pop()
+                postfix.append(z)
+            except:
+                z = 0
+            stack.append(stackMod(x,y,z))
+        
         elif token == "+":
-            x = stack.pop()
-            y = stack.pop()
-            stack.append(str(int(x) + int(y)))
+            z = 0
+            x = postfix.pop()
+            if x == " ":
+                x = postfix.pop()
+            y = postfix.pop()
+            if y == " ":
+                y = postfix.pop()
+            try:
+                z = postfix.pop()
+                if z == " ":
+                    z = postfix.pop()
+                postfix.append(z)
+            except:
+                z = 0
+            stack.append(stackAdd(x,y,z))
+        
         elif token == "-":
-            x = stack.pop()
-            y = stack.pop()
-            stack.append(str(int(x) - int(y)))
+            z = 0
+            x = postfix.pop()
+            if x == " ":
+                x = postfix.pop()
+            y = postfix.pop()
+            if y == " ":
+                y = postfix.pop()
+            try:
+                z = postfix.pop()
+                if z == " ":
+                    z = postfix.pop()
+                postfix.append(z)
+            except:
+                z = 0
+            stack.append(stackSub(x,y,z))
+            
+            #I genuinely have no idea why "5" == ")". Anyhow, commented here for posterity's sake.
+        '''#Parentheses (if any) should be acknowledged.
         elif token == "(" or ")":
-            print("A Critical Error has Occurred on line 128. Reason: Unacceptable Token: ( or ).")
-            break
+            print("TOKEN: " + token)
+            print("A Critical Error has Occurred on line 146. Reason: Unacceptable Token: ( or ).")
+            break'''
+        
     if len(stack) > 1:
         stack_string = ""
         for element in stack:
-            stack_string.append(element)
-        print("A Critical Error has Occurred on line 134. Reason: End of Expression Evaluation, but Stack has more than 1 argument. Stack: " + stack_string + " Postfix: " + postfix)
-        return
+            stack_string += element + " "
+        print("A Critical Error has Occurred on line 190. Reason: End of Expression Evaluation, but Stack has more than 1 argument. Stack: " + stack_string + " Postfix: " + str(postfix))
+    elif len(stack) < 1:
+        print("A Critical Error has Occurred on line 192. Reason: End of Expression Evaluation, but Stack is Empty. Postfix: " + str(postfix))
     else:
-        answer = int(stack[0])
-        return answer
+        return int(stack[0])
 
 
 
@@ -157,11 +332,20 @@ def getStdInput():
 
 '''SECTION THREE: DATA FUNCTIONS'''
 
+#An evil made necessary by my own stupidity.
+#I misinterpretted the instructions and thought postfix was supposed to be a string, not a stack.
+#Hence, this function is invoked.
+def string2stack(string):
+    stack = []
+    for token in string:
+        stack.append(token)
+    return stack
+
 #Converts a dictionary to a plain string of keys and values.
 def getStringOutput(dictionary):
     dict_string = ""
     for key in dictionary:
-        dict_string.append(key + " = " + str(dictionary[key]) + "\n")
+        dict_string += (key + " = " + str(dictionary[key]) + "\n")
     return dict_string
 
 #Takes infix strings, passes them to conversion, passes them to eval, and returns a dictionary.
@@ -169,9 +353,11 @@ def getAnswersDict(strings):
     answers={}
     new_eq = ""
     new_answer = 0
+    new_stack = []
     for line in strings:
         new_eq = infix2postfix(line)
-        new_answer = evalPostfix(new_eq)
+        new_stack = string2stack(new_eq)
+        new_answer = evalPostfix(new_stack)
         answers[new_eq] = new_answer
     return answers
 
@@ -188,7 +374,6 @@ def main():
     else:
         infix_strings = getStdInput()
     print(getStringOutput(getAnswersDict(infix_strings)))
-    return
 
 #For modularity, this is the only procedural script.
 if __name__ == '__main__':
